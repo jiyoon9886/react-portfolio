341af90b3b2e990534244b65030dcf13a308f3f9
Jiyoon Kim <jiyoon9886@gmail.com>
2020-06-23 20:45:45 -0400

    deployment

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 //import { Container, Row, Col } from 'react-bootstrap';
 // import Header from "./components/Header";
 // import Footer from "./components/Footer";
-import Home from './pages/Home.js';
-import Projects from './pages/Projects.js';
-import Navbar from './components/Navbar.js';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Navbar from './components/Navbar';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
