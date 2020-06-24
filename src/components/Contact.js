08d9d9b5316b9b40169f814afa9b808710e02966
Jiyoon Kim <jiyoon9886@gmail.com>
2020-06-24 12:31:29 -0400

    added links to icon buttons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 import GitHubIcon from '@material-ui/icons/GitHub';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { IconButton } from '@material-ui/core';
 
 const Contact = () => {
 	return (
-    
-    <div style={{ textAlign: 'center' }}>
-      <Link to='https://github.com/jiyoon9886'>
-        <GitHubIcon />
-      </Link>
+    <div style={contactIcons}>
+      <IconButton href='https://github.com/jiyoon9886'>
+        <GitHubIcon className='material-icons md-dark' />
+      </IconButton>
+      <IconButton href='https://www.linkedin.com/in/ji-yoon-kim-161a001a4/'>
+        <LinkedInIcon className='material-icons md-dark' />
+      </IconButton>
+      <IconButton href='https://www.instagram.com/jiyoonest/'>
+        <InstagramIcon className='material-icons md-dark' />
+      </IconButton>
+      <IconButton href='mailto:jiyoon9886@gmail.com'>
+        <MailOutlineIcon className='material-icons md-dark' />
+      </IconButton>
       <p>I look forward to meeting you!</p>
     </div>
-
-
   );
 }
 
 export default Contact;
+
+const contactIcons = {
+  textAlign: 'center',
+};
