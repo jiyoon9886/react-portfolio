873898251a6dca44c035d1d12fe8fbb8eb839143
Jiyoon Kim <jiyoon9886@gmail.com>
2020-06-24 03:11:33 -0400

    added scripts and modified routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-//import { Container, Row, Col } from 'react-bootstrap';
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
 import Home from './pages/home';
 import Projects from './pages/projects';
 import Navbar from './components/Navbar';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <Router>
       <div className='App'>
-        {/* <Route path='/' component={Home} /> */}
         <Navbar />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/projects' component={Projects} />
-        </Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/projects' component={Projects} />
       </div>
     </Router>
   );
