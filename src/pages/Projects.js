38713d76c6fa59b6f400a0474ee51a6457362fa0
Jiyoon Kim <jiyoon9886@gmail.com>
2020-06-24 02:58:58 -0400

    deploy

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,41 +1,36 @@
-import React from "react";
+import React from 'react';
 
-import ProjectCard from "../components/ProjectCard.js";
+import ProjectCard from '../components/ProjectCard.js';
 import Grid from '@material-ui/core/Grid';
 
-import myProjects from "../utils/myProjects";
-
-
+import myProjects from '../utils/myProjects';
 
 const Projects = () => {
-    return (
-			<Grid
-				container
-				style={container}
-				justify='center'
-				alignItems='center'
-				direction='column'
-			>
-				
-        {myProjects.map((project) => (
-          <ProjectCard 
-						image={project.image}
-						title={project.title}
-						description={project.description}
-						repo={project.repo}
-						demo={project.demo}
-					/>
-        ))}
-					
-      </Grid>
-			
-		);	
-}
+  return (
+    <Grid
+      container
+      style={container}
+      justify='center'
+      alignItems='center'
+      direction='column'
+    >
+      {myProjects.map((project) => (
+        <ProjectCard
+          image={project.image}
+          title={project.title}
+          description={project.description}
+          repo={project.repo}
+          demo={project.demo}
+        />
+      ))}
+    </Grid>
+  );
+};
 
 export default Projects;
 
 const container = {
-	width: '100vw',
-	height: '100vh',
-	flewgrow: '1',
+  width: '100vw',
+  height: '100vh',
+  flewgrow: '1',
 };
